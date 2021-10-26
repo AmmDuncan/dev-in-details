56255672721d6eb2d2153fba0d2e1f839e41ef22
ammduncan <ammduncan@gmail.com>
2021-10-26 22:21:56 +0000

    added global styles

---
+++ styles/globalStyle.js
@@ -0,0 +1,23 @@
+import {createGlobalStyle} from "styled-components";
+
+export default createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    font-family: "Inter", "Segoe UI", Helvetica, sans-serif;
+  }
+  
+  h1, h2, h3, h4 {
+    font-family: "Overpass", "Segoe UI", Helvetica, sans-serif;
+    line-height: 1;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
